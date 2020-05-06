bf76e95321fd3e9f378d2f2fbbb4cf6ef2f3caf6
tonystark2005 <tonystark2005@github.com>
2020-05-06 00:24:10 -0400

    Updates

---
+++ precache-manifest.9b5e932a2cb8feb8143e44b9014ccda8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d089bc83fa7660abf359f9eb76b8f82",
+    "url": "/YouTube-Clone/index.html"
+  },
+  {
+    "revision": "ef43288d327fa64bff5e",
+    "url": "/YouTube-Clone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cde52f7fe847a828271",
+    "url": "/YouTube-Clone/static/js/2.84f6364c.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/YouTube-Clone/static/js/2.84f6364c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef43288d327fa64bff5e",
+    "url": "/YouTube-Clone/static/js/main.043ef9ba.chunk.js"
+  },
+  {
+    "revision": "2ee9ee47ad588c4ddf6f",
+    "url": "/YouTube-Clone/static/js/runtime-main.b95d1be2.js"
+  }
+]);
