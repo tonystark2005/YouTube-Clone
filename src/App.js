5b51124b23852e7facdabd628296992078a1d743
tonystark2005 <tonystark2005@github.com>
2021-05-03 12:08:53 -0500

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { UI, YoutubeUI } from "./components";
+import { HomePage, UI } from "./components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route path="/" exact component={YoutubeUI} />
+        <Route path="/" exact component={HomePage} />
         {/* <Route path="/test" exact component={test} /> */}
         <Route path="/ui" exact component= {UI} />
       </Switch>
-      {/* <div>
-        <UI></UI>
-      </div>
-      <div>
-        <test></test>
-      </div> */}
     </Router>
   );
 }
