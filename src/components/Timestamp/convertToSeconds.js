e5cfe785692f0aebbb6e7698944f4c20ca352ff7
tonystark2005 <tonystark2005@github.com>
2021-05-03 16:25:05 -0500

    rename variables

--- src/components/Timestamp/convertToSeconds.js
+++ src/components/Timestamp/convertToSeconds.js
@@ -1,15 +1,15 @@
 const convertToSeconds = time => {
     if (time === null) return;
-    var a = time[0].split(":");
-    var seconds;
-    if (a.length === 3) {
-        return seconds = +a[0] * 60 * 60 + +a[1] * 60 + +a[2];
+    time = time[0].split(":");
+    let seconds;
+    if (time.length === 3) {
+        return seconds = +time[0] * 60 * 60 + +time[1] * 60 + +time[2];
     }
-    if (a.length === 2) {
-        return seconds = +a[0] * 60 + +a[1];
+    if (time.length === 2) {
+        return seconds = +time[0] * 60 + +time[1];
     }
-    if (a.length === 1) {
-        return seconds = a[0];
+    if (time.length === 1) {
+        return seconds = time[0];
     }
     console.log(seconds);
     return seconds;
