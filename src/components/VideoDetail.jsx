86364499ca2960e5247eccecf91e8b2ea7f3e142
tonystark2005 <tonystark2005@github.com>
2021-05-03 12:25:05 -0500

    remove comments and add styles

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,44 +1,31 @@
 import React, { useRef } from "react";
 
 import { Paper, Typography, Box } from "@material-ui/core";
-import AutoDetect from "./AutoDetect";
+import AutoDetect from "./Timestamp/DetectTimestamp";
 import ReactPlayer from "react-player";
 
 export default ({ video }) => {
-  if (!video) return <Typography style={{ justifyContent: "center" }}>Loading</Typography>;
-  // const [newVideo, setnewVideo] = useState(null);
+  if (!video) return <Typography variant="h3" className="loading">Loading...</Typography>;
   const videoSrc = `https://www.youtube.com/embed/${video.id}?enablejsapi=1`;
   const player = useRef(null);
-  // useEffect(() => {
-  //   player.current.focus();
-  // }, []);
-  // try {
-  //   player.seekTo(45);
-  // } catch (error) {}
 
   return (
     <React.Fragment>
-      <Paper elevation={6} style={{ height: "100%" }}>
-        <ReactPlayer
-          width="100%"
-          height="50%"
-          ref={player}
-          url={videoSrc}
-          controls={true}
-          playing={true}
-          // autoPlay={true}
-          // config
-        ></ReactPlayer>
-        {/* <Button onClick={() => player.current.seekTo(45)}>click me</Button> */}
-        {/* <iframe
-          frameBorder="0"
-          width="932px"
-          height="524px"
-          title="Video Player"
-          src={videoSrc}
-          allowFullScreen
-        /> */}
-        <Box elevation={6} style={{ padding: "5px" }}>
+      <Paper>
+        <div className="player-wrapper">
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            ref={player}
+            url={videoSrc}
+            controls={true}
+            playing={true}
+            // autoPlay={true}
+            // config
+            className="react-player"
+          ></ReactPlayer>
+        </div>
+        <Box style={{ padding: "5px" }}>
           <Typography variant="h5">
             {video.snippet.title} - {video.snippet.channelTitle}
           </Typography>
@@ -48,10 +35,6 @@ export default ({ video }) => {
           <Box>
             <AutoDetect video={video} player={player}></AutoDetect>
           </Box>
-          {/* <Typography>
-            <Test sections={sections}></Test>
-            <Test handleTimestamp={handleTimestamp}></Test>
-          </Typography> */}
         </Box>
       </Paper>
     </React.Fragment>
