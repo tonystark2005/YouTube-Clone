d6a706f4cbb3ce4df38364833c65d991e95743e1
tonystark2005 <tonystark2005@github.com>
2021-05-03 12:13:08 -0500

    add params

--- src/components/api/youtube.js
+++ src/components/api/youtube.js
@@ -1,5 +1,12 @@
 import axios from "axios";
+const API_KEY = "AIzaSyCw93QMRp0exJUTSXql_1rB6EFajvSuIdU";
 
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3"
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 10,
+    type: "video",
+    key: API_KEY,
+  }
 });
